e4a26e9ab93e12377612b9b3294077c3a74c55fc
Pavel9172 <pavlo_k@mail.ru>
2022-03-18 15:30:00 +0500

    new project

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args){
+        int omount = 780;
+        int mile = 20;
+
+        int bonus = omount / mile;
+
+        System.out.println(" Ваши мили "  + bonus);
+
+    }
+
+}
