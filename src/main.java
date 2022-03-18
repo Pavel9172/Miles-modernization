e3a750673cac6709b808cf3410eb715d27029b75
Pavel9172 <pavlo_k@mail.ru>
2022-03-18 20:07:21 +0500

    new data

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 public class main {
     public static void main(String[] args){
+        // стоимость билета
         int omount = 780;
+        // неизменяемые условия
         int mile = 20;
 
         int bonus = omount / mile;
