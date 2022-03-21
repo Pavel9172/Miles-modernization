a3a77467254d09ea68b66e7477d8744fd0032322
Pavel9172 <pavlo_k@mail.ru>
2022-03-21 20:31:04 +0500

    code 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-public class main {
+public class Main {
     public static void main(String[] args){
         // стоимость билета
-        int omount = 780;
+        int amount = 780;
         // неизменяемые условия
         int mile = 20;
 
-        int bonus = omount / mile;
+        int bonus = amount / mile;
 
         System.out.println(" Ваши мили "  + bonus);
 
